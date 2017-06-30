cbb622616384427537e59d189a499785df2d1f03
mayuriyam <mayuriya21@gmail.com>
2017-06-30 11:31:50 +0530

    Create strconv.java

---
+++ strconv.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+class strconv
+{
+    public static void main(String ar[])
+    {
+    Scanner sc=new Scanner(System.in);
+    String a=sc.next();
+    int b=Integer.valueOf(a);
+    
+    System.out.println(b);
+    
+    }
+}
